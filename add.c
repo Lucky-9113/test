736243b788ed08f381a41bbb85054ff33f5b8e61
Ubuntu <ubuntu@ip-172-31-33-63.us-east-2.compute.internal>
2023-02-16 08:41:53 +0000

    modififed

--- add.c
+++ add.c
@@ -6,9 +6,11 @@ int main() {
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
-    // calculating sum
+    
     sum = number1 + number2;      
     
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
 }
+
+
